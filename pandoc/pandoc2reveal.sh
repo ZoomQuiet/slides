90bec33ecac446236d0c89529f1811e84866e5c4
Zoom.Quiet <zoom.quiet@gmail.com>
2025-05-14 17:22:45 +0800

    🎨 UPDATE:

--- pandoc/pandoc2reveal.sh
+++ pandoc/pandoc2reveal.sh
@@ -1,24 +1,99 @@
 #!/bin/sh
 #=========================================================== var defines
-VER="pandoc2reveal v190106.1353"
-echo $VER
-echo USAGE::
-echo '  pandoc2reveal.sh "[源文稿名]"'
-echo '  NOT .md'
+VER="pandoc2reveal v.250514.13542"
+
+# 定义主题映射
+declare -a THEME_MAP=(
+    [0]="black"
+    [1]="white"
+    [2]="league"
+    [3]="beige"
+    [4]="night"
+    [5]="serif"
+    [6]="simple"
+    [7]="solarized"
+    [8]="moon"
+    [9]="dracula"
+    [10]="sky"
+    [11]="blood"
+)
+
+# 定义函数来显示主题映射
+show_themes() {
+    echo "\n主题编号与对应的主题名称如下："
+    for key in "${!THEME_MAP[@]}"; do
+        echo "$key : ${THEME_MAP[$key]}"
+    done
+}
+# 检查参数数量是否正确
+if [ $# -ne 2 ]; then
+    echo '\n USAGE::'
+    echo ' 进入pandoc2reveal.sh 所在目录'
+    echo '$ ./pandoc2reveal.sh <文件名> <主题编号>'
+    echo '  <文件名> 不含.md 后缀'
+    echo '编号范围: 0..11 对应官方12种主题'
+    show_themes
+    exit 1
+fi
+# 获取输入参数
+FILENAME="$1"
+THEME_NUMB="$2"
+
+# 检查主题编号是否有效
+if [[ "$THEME_NUMB" =~ ^[0-9]+$ ]] && [ "$THEME_NUMB" -ge 0 ] && [ "$THEME_NUMB" -le 11 ]; then
+    SELECTED_THEME="${THEME_MAP[$THEME_NUMB]}"
+else
+    show_themes
+    echo "错误: 主题编号必须为 0-11 之间的整数"
+    exit 1
+fi
 
 AIMP='../public'
-NAME=$1
-if [ -z $NAME ] ;then    
-    echo ALERT::
-    echo '  LOST "[源文稿名]"'
-    exit  0
-else
-
-echo base $NAME
+#NAME=$1
+#if [ -z $NAME ] ;then    
+#    echo ALERT::
+#    echo '  LOST "[源文稿名]"'
+#    exit  0
+#else
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo base $FILENAME
 MD=$1.md
 SLIDES=$AIMP/$1.html
 echo "got draft <-- $MD"
 echo "exp. reveal.js slides -> $SLIDES"
+#exit  0
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+# 构建 pandoc 命令字符串，使用多行定义
+cmd="pandoc \"$MD\" \
+-o \"$SLIDES\" \
+--standalone \
+--css=custom.css \
+-t revealjs \
+-V revealjs-url=./reveal.js \
+-V slideNumber=true \
+-V progress=true \
+-V transition=zoom \
+-V theme=\"$SELECTED_THEME\" "
+#--standalone \
+#-t revealjs \
+#-V revealjs-url=./reveal.js \
+#-V margin=0.01 \
+#-V slideNumber=true \
+#-V progress=true \
+#--css=custom.css \
+#-V transition=zoom \ fade,slide,convex,concave,zoom
+#-V theme=serif 
+
+
+# 打印命令
+echo "即将执行的命令："
+echo "$cmd"
+echo
+
+# 执行命令
+eval "$cmd"
+
 #=========================================================== path defines
 #PY=$( which python)
 #NOW_TIME_STAMP=$(date "+%y%m%d.%H%M%S")
@@ -31,18 +106,14 @@ echo "exp. reveal.js slides -> $SLIDES"
 #echo "###::run@" `date +"%Y/%m/%d %H:%M:%S"` >> $LOGD
 #echo "<<<   trigger by inter. API srv."  >> $LOGD
 
-echo pandoc $MD -o $SLIDES -t revealjs -s -V theme=solarized
-
-pandoc $MD -o $SLIDES -t revealjs -s -V theme=solarized
+#echo pandoc $MD -o $SLIDES -t revealjs -s -V theme=solarized
+#pandoc $MD -o $SLIDES -t revealjs -s -V theme=solarized
 
 #echo "###::end@" `date +"%Y/%m/%d %H:%M:%S"` >> $LOGD
 #echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"  >> $LOGD
 #=========================================================== action DONE
-
-fi #if [ -z $NAME ] ;then
-
-# pandoc "ctalk220908gdgzh.md" -o "ctalk220908gdgzh.pdf" --pdf-engine=xelatex --toc --toc-depth=4 --from markdown 
---template eisvogel --listings
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo $VER
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 exit  0
-
---
+++ pandoc2slide.md
@@ -0,0 +1,117 @@
+% Pandoc 2 Slides
+% Zoom.Quiet
+% 181230~230319
+
+# Why
+
+## 幻灯
+
+为什么学?
+
+## 高桥流
+> 技不压身
+
+## S5
+
+> - html+js+css
+> - 手工 html
+> - rst2s5
+> - markdown -> s5
+
+## 嗯哼
+
+- 更加现代
+- 更加快捷
+- 更加...
+
+# What
+
+## 一键生成
+为什么?
+
+## pages 兼容
+> 技不压身
+
+## 内置模板够美
+> 不调CSS3
+
+# How
+
+## 安装
+> pip install pandoc
+
+## 使用
+
+    pandoc pandoc2slide.md \
+        -o pandoc2slide.html \
+        -t revealjs -s
+
+## 工具化:
+> 自制小脚本
+
+
+>- 编译 md -> html
+>    + $ ./pandoc2reveal.sh AIGCxZh-life3 7
+>    + [配套幻灯编译脚本](https://gist.github.com/ZoomQuiet/cba81f15ecd6c39d7ffae37272e1ad8f)
+>- 调用浏览器 app 模式本地演示
+>    + $ cr4app AIGCxZh-life3.html
+>    + [配置本地演示脚本](https://gist.github.com/ZoomQuiet/008989ec91e65a3ba70fed32b88f5623)
+
+## 发布
+
+>- [plain-html · GitLab](https://gitlab.com/pages/plain-html/)
+>- [About GitHub Pages - GitHub Docs](https://docs.github.com/en/pages/getting-started-with-github-pages/about-github-pages#types-of-github-pages-sites)
+>- [Introduction to Vercel | Vercel Docs](https://vercel.com/docs) ..
+
+## TODO
+
+> - 自动化
+> - 可视化?
+> - 定制样式 [Themes | reveal.js](https://revealjs.com/themes/), [themes.dzello](https://revealjs-themes.dzello.com/#/)
+> - Slack 结合
+> - ...
+
+# (￣▽￣)
+
+## 눈_눈
+> 是也乎
+
+>- [101camp/slides: slides.101.camp collections all presentations from DAMA](https://github.com/101camp/slides)
+>- **https://slides.101.camp**
+
+-------
+
+![ask**DAMA**@**g**oo**g**le**g**roup**s**.com](http://org.up.zoomquiet.top/omc/res/KEEP/kcn_ask-dama.jpg!/fh/420)
+
+## 是也乎
+
+- 231212 append usage
+- 230315 mv into Vercel
+- 221216 mv into GitHub
+- 190106 @auto-md Leo
+- 181230 subl make
+
+
+-------
+
+![](img/190416got-ride-dragon.jpg)
+
+## MVP
+
+> 开发/调试的 最小行为
+
+    编写/修改
+    ^   +->运行
+    |       +-> 观察
+    \__________/
+
+## Change
+
+> 在线可阅
+
+[slides.101.camp/d0d0ch](http://slides.101.camp/d0d0ch.html)
+
+
+------
+
+![Zzz...](http://org.up.zoomquiet.top/omc/res/KEEP/kcn_sleep.png?imageView2/2/w/510)
